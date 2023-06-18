f48820283986ea9a8816f57e59acd23a239b0f88
nikasuschinskaya@gmail.com <nikasuschinskaya@gmail.com>
2023-06-18 23:02:12 +0300

    add main views

--- VendingMachine/VendingMachine.UI/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ VendingMachine/VendingMachine.UI/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace VendingMachine.UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VendingMachine.UI;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VendingMachine.UI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
